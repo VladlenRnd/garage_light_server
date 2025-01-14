baaeab249150acca63fe430b09f122543de271ad
Eners <vladlen.pleshenkov@w-src.com>
2025-01-14 23:11:27 +0200

    change port

--- bin/server.dart
+++ bin/server.dart
@@ -97,6 +97,6 @@ void main(List<String> args) async {
   final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router.call);
 
   //final server = await shelf_io.serve(handler, '192.168.0.111', 8080);
-  final server = await shelf_io.serve(handler, '0.0.0.0', 223);
+  final server = await shelf_io.serve(handler, '0.0.0.0', 3000);
   _log.info('Сервер запущен на http://${server.address.host}:${server.port}');
 }
